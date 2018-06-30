0cd7868c5131ed1801c1528f3a2bd6136f3f7957
游品尧 <497400448@qq.com>
2018-06-30 21:50:11 +0800

    perf: file.js

--- src/util/file.js
+++ src/util/file.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 
+let count = 0;
+
 module.exports = {
   read(path) {
+    count ++;
     return new Promise((resolve, reject) => {
       let chunks = '';
+      const fd = +(new Date()) + count;
       const readable = fs.createReadStream(path, {
         autoClose: true,
+        fd,
       });
       readable.on('data', function (chunk) {
         chunks += chunk;
@@ -13,20 +18,33 @@ module.exports = {
 
       readable.on('end', function () {
         resolve(chunks);
+        try {
+          fs.close(fd);
+        } catch (error) {
+          console.log('fs.close', error);
+        }
       });
       readable.on('error', reject);
     });
   },
   write(path, content) {
+    count ++;
     return new Promise((resolve, reject) => {
-      const wstream = fs.createWriteStream(path);
+      const fd = +(new Date()) + count;
+      const wstream = fs.createWriteStream(path, {
+        autoClose: true,
+        fd,
+      });
 
       wstream.on('open', () => {
         wstream.write(content);
         wstream.end();
       });
       wstream.on('error', reject);
-      wstream.on('finish', resolve);
+      wstream.on('finish', () => {
+        fs.close(fd);
+        resolve();
+      });
     });
   }
 }
