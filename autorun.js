16d6d43909bebceee96b0db862c76ffcac25abaf
游品尧 <497400448@qq.com>
2018-07-05 16:15:39 +0800

    perf: clearCache

--- autorun.js
+++ autorun.js
@@ -26,7 +26,7 @@ let isRuning = false;
 async function run() {
   isRuning = true;
 
-  spawn.sync('npm', ['run', 'clear-cache'], options);
+  spawn.sync('npm', ['run', 'clear-cache-all'], options);
   spawn.sync('npm', ['run', 'pick'], options);
   spawn.sync('npm', ['run', 'serve:stop'], options);
   spawn.sync('npm', ['run', 'serve'], options);
@@ -57,7 +57,7 @@ async function check() {
     fs.writeFileSync(dateCachePath, dataCache);
     run();
   } else {
-    console.log(chalk.yellow(`dataCache ${dataCache} | currentDate ${currentDate}`));
+    console.log(chalk.yellow(`[${+new Date()}] dataCache ${dataCache} | currentDate ${currentDate}`));
   }
 }
 
